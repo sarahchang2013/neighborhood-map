9f07e68589481067d84151d0c7eb40682c39a60b
sarahchang2013 <sarah_sarah99@hotmail.com>
2017-12-28 13:04:35 +0800

    bind list to locations in knockout

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,35 @@ function initMap() {
 		zoom: 13
     });
 }
+
+locations = [
+	{
+		name: "National Library of Serbia"
+	},
+	{
+		name: "Church of Saint Sava"
+	},
+	{
+		name: "Old Palace"
+	},
+	{
+		name: "Belgrade City Library"
+	},
+	{
+		name: "Kalemegdan Park"
+	}
+]
+
+var Location = function(data) {
+	this.name = ko.observable(data.name);
+}
+
+var ViewModel = function() {
+	var self = this;
+	self.locationList = ko.observableArray([]);
+	for (var i=0; i<locations.length; i++) {
+		self.locationList.push(new Location(locations[i]));
+	}
+}
+
+ko.applyBindings(new ViewModel());
